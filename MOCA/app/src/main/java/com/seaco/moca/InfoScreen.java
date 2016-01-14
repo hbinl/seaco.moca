93681595e711205cc3bbbeb04332d6100296276d
Hao Bin <imloh7@gmail.com>
2016-01-14 17:05:41 +0800

    Added paging system and more layout bugfixes

--- MOCA/app/src/main/java/com/seaco/moca/InfoScreen.java
+++ MOCA/app/src/main/java/com/seaco/moca/InfoScreen.java
@@ -1,18 +1,35 @@
 package com.seaco.moca;
 
 import android.app.AlertDialog;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.net.Uri;
+
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 public class InfoScreen extends AppCompatActivity {
+
+//    FragmentManager fragmentManager;
+    RelativeLayout container;
+    LayoutInflater layoutInflater;
+    static int pageIndex;
+    static Integer[] pageArray;
+    FloatingActionButton fab_forward;
+    FloatingActionButton fab_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,35 +37,76 @@ public class InfoScreen extends AppCompatActivity {
         setContentView(R.layout.activity_info_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //getSupportActionBar().setTitle("Get Ready");
 
-
-
-
-        final FloatingActionButton fab_back = (FloatingActionButton) findViewById(R.id.fab_back);
+        fab_back = (FloatingActionButton) findViewById(R.id.fab_back);
         fab_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                goBackHome();
+                Snackbar.make(view, "Go back", Snackbar.LENGTH_SHORT)
+                        .setAction("Next", null).show();
+                proceedToPreviousScreen();
             }
         });
 
-        FloatingActionButton fab_forward = (FloatingActionButton) findViewById(R.id.fab_forward);
+        fab_forward = (FloatingActionButton) findViewById(R.id.fab_forward);
         fab_forward.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Proceed", Snackbar.LENGTH_SHORT)
                         .setAction("Next", null).show();
-                if (fab_back.getVisibility() == View.INVISIBLE) {
-                    fab_back.setVisibility(View.VISIBLE);
-                } else if (fab_back.getVisibility() == View.VISIBLE) {
-                    fab_back.setVisibility(View.INVISIBLE);
-                }
-
+                proceedToNextScreen();
             }
         });
 
+        //fab_back = (FloatingActionButton) findViewById(R.id.fab_back);
         fab_back.setVisibility(View.INVISIBLE);
+
+        initializePages();
+
+        container = (RelativeLayout) findViewById(R.id.moca_tests_container);
+        View child = getLayoutInflater().inflate(pageArray[pageIndex],null);
+        container.addView(child);
+
+
+    }
+
+    private void initializePages() {
+        pageIndex = 0;
+        pageArray = new Integer[] {
+                R.layout.moca_overview,
+                R.layout.moca_visuo_node
+        };
+    }
+
+    private void proceedToScreenLayout(int screen) {
+        // screen = R.layout.content_info_screen
+        container.removeAllViews();
+        View child = getLayoutInflater().inflate(screen ,null);
+        container.addView(child);
+    }
+
+    private void proceedToNextScreen() {
+        if (pageIndex + 1 < pageArray.length) {
+            proceedToScreenLayout(pageArray[pageIndex+1]);
+            fab_back.setVisibility(View.VISIBLE);
+            pageIndex += 1;
+        }
+        if (pageIndex == pageArray.length - 1) {
+            fab_forward.setVisibility(View.INVISIBLE);
+        }
+
+
+    }
+
+    private void proceedToPreviousScreen() {
+        if (pageIndex - 1 >= 0) {
+            proceedToScreenLayout(pageArray[pageIndex-1]);
+            fab_forward.setVisibility(View.VISIBLE);
+            pageIndex -= 1;
+        }
+        if (pageIndex == 0) {
+            fab_back.setVisibility(View.INVISIBLE);
+        }
     }
 
     @Override
@@ -57,6 +115,8 @@ public class InfoScreen extends AppCompatActivity {
         inflater.inflate(R.menu.screen_menu, menu);
         return true;
     }
+
+
 
     @Override
     public void onBackPressed() {
